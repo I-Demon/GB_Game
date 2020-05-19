562a4f31ba23bac92d9325e645441bcc86102aad
dshu <dshu77@gmail.com>
2020-05-19 22:39:22 +0300

    Начало

---
+++ core/src/dune/game/DuneGame.java
@@ -0,0 +1,14 @@
+package dune.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+public class DuneGame extends ApplicationAdapter {
+
+}
