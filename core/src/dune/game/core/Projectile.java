be5458e04319555b0025c916d3c1d9d975a6a8fd
dshu <dshu77@gmail.com>
2020-05-23 12:41:37 +0300

    Домашнее задание по уроку 2

--- core/src/dune/game/core/Projectile.java
+++ core/src/dune/game/core/Projectile.java
@@ -1,5 +1,6 @@
 package dune.game.core;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
@@ -8,26 +9,37 @@ import com.badlogic.gdx.math.Vector2;
 public class Projectile {
     private Vector2 position;
     private Vector2 velocity;
-    private Texture textures;
+    private TextureRegion textures;
     private float angle;
     private float speed;
 
     public Projectile(TextureAtlas atlas, float x, float y) {
         this.position = new Vector2(x, y);
-        this.textures = new TextureRegion(atlas.findRegion("bullet")).split(64, 64)[0];
+        this.textures = new TextureRegion(atlas.findRegion("bullet"));
         this.speed = 300.0f;
-        this.timePerFrame = 0.08f;
+        this.velocity =   position.cpy(); //.add(0,10); // new Vector2(10, 10);
     }
 
     public void setup(Vector2 startPosition, float angle) {
-        velocity.set(100.0f * MathUtils.cosDeg(angle), 0.0f);
+        velocity.set(speed * MathUtils.cosDeg(angle), speed  * MathUtils.sinDeg(angle));
 
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
     }
 
     public void update(float dt) {
-        // position.x += velocity.x * dt;
-        // position.y += velocity.y * dt;
-        // position.mulAdd(velocity, dt);
+         position.x += velocity.x * dt;
+         position.y += velocity.y * dt;
+         position.mulAdd(velocity, dt);
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(textures, position.x - 8, position.y - 8, 8, 8, 16, 16, 1, 1, angle);
     }
 }
-
