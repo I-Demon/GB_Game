bcd15fc15d190d4b8b147af913313cd31a3b68d2
dshu <dshu77@gmail.com>
2020-06-09 22:22:15 +0300

    Домашнее задание по уроку 7

--- core/src/dune/game/core/gui/GuiPlayerInfo.java
+++ core/src/dune/game/core/gui/GuiPlayerInfo.java
@@ -7,6 +7,40 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.StringBuilder;
 import dune.game.core.PlayerLogic;
 
-public class GuiPlayerInfo extends Group {//
+public class GuiPlayerInfo extends Group {
+    private PlayerLogic playerLogic;
+    private Label money;
+    private Label unitsCount;
+    private Label score;        // счет собранных ресурсов
+    private StringBuilder tmpSB;
 
+    public GuiPlayerInfo(PlayerLogic playerLogic, Skin skin) {
+        this.playerLogic = playerLogic;
+        this.money = new Label("", skin, "simpleLabel");
+        this.unitsCount = new Label("", skin, "simpleLabel");
+        this.money.setPosition(10, 10);
+        this.unitsCount.setPosition(210, 10);
+
+        this.score = new Label("", skin, "simpleLabel");  //dshu вывод инфо о текущем счете
+        this.score.setPosition(310,10);
+        this.addActor(money);
+        this.addActor(unitsCount);
+        this.addActor(score);
+        this.tmpSB = new StringBuilder();
+    }
+
+    public void update(float dt) {
+        tmpSB.clear();
+        tmpSB.append("MINERALS: ").append(playerLogic.getMoney());
+        money.setText(tmpSB);
+        tmpSB.clear();
+        tmpSB.append("UNITS: ").append(playerLogic.getUnitsCount()).append(" / ").append(playerLogic.getUnitsMaxCount());
+        unitsCount.setText(tmpSB);
+
+
+        // dshu выводим информацию о счете
+        tmpSB.clear();
+        tmpSB.append("SCORE: ").append(playerLogic.getScore());
+        unitsCount.setText(tmpSB);
+    }
 }
--- core/src/dune/game/screens/MenuScreen.java
+++ core/src/dune/game/screens/MenuScreen.java
@@ -22,6 +22,93 @@ import dune.game.core.units.AbstractUnit;
 
 //
 
-public class MenuScreen extends AbstractScreen { //
+public class MenuScreen extends AbstractScreen {
+    private Stage stage;
+    private GameController gameController;
+    private WorldRenderer worldRenderer;
 
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public MenuScreen(SpriteBatch batch) {
+        super(batch);
+
+
+    }
+
+    @Override
+    public void show() {
+        //this.gameController = new GameController();
+        //this.worldRenderer = new WorldRenderer(batch, gameController);
+        createGui();
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+        Gdx.gl.glClearColor(0.4f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+    }
+
+    public void update(float dt) {
+       /*
+        if (Gdx.input.justTouched()) {
+            ScreenManager.getInstance().changeScreen(ScreenManager.ScreenType.GAME);
+        }
+
+        */
+
+       stage.act(dt);
+
+    }
+
+
+
+    public void createGui() {
+        //dshu Рисуем кнопки на экране меню для начала игры и выхода из игры
+
+        stage = new Stage(ScreenManager.getInstance().getViewport(), ScreenManager.getInstance().getBatch());
+
+        Gdx.input.setInputProcessor(stage);
+        Skin skin = new Skin();
+        skin.addRegions(Assets.getInstance().getAtlas());
+        BitmapFont font14 = Assets.getInstance().getAssetManager().get("fonts/font14.ttf");
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle(
+                skin.getDrawable("smButton"), null, null, font14);
+        final TextButton StartBtn = new TextButton("Start Game", textButtonStyle);
+        StartBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ScreenManager.getInstance().changeScreen(ScreenManager.ScreenType.GAME);
+            }
+        });
+
+        final TextButton exitBtn = new TextButton("Exit", textButtonStyle);
+        exitBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+
+        Group menuGroup = new Group();
+        StartBtn.setPosition(0, 50);
+        exitBtn.setPosition(0, 0);
+        menuGroup.addActor(StartBtn);
+        menuGroup.addActor(exitBtn);
+        menuGroup.setPosition(600, 400);
+
+        stage.addActor(menuGroup);
+
+        skin.dispose();
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
